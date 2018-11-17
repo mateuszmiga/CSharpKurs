3e2b57d03cdfa4d292fdc11e09be1dfb58abc5b8
mateuszmiga <mateusz.miga11@gmail.com>
2018-11-17 08:06:39 +0100

    aktualizacja

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -8,8 +8,65 @@ namespace Hello
 {
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
+			ValueTypes();
+			Greeting();
+			for (; ; )
+			{
+				Age();
+			}
+
+		}
+
+		private static void ValueTypes()
+		{
+			int maxInt = int.MaxValue;
+			int minInt = int.MinValue;
+			long maxLong = long.MaxValue;
+			long minLong = long.MinValue;
+			Console.WriteLine("maxInt = " + maxInt);
+			Console.WriteLine("minInt = " + minInt);
+			Console.WriteLine("maxLong = " + maxLong);
+			Console.WriteLine("minLong = " + minLong);
+		}
+
+		/// <summary>
+		/// komunikat zależny od wieku
+		/// </summary>
+		private static void Age()
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("ile masz lat? ");
+			int age;
+			bool result = int.TryParse(Console.ReadLine(), out age);
+
+			if (age < 18 && age > 0)
+			{
+				Console.ForegroundColor = ConsoleColor.Magenta;
+				Console.WriteLine("za mlody");
+			}
+			else if (result == false)
+			{
+				Console.WriteLine("podaj poprawny wiek");
+			}
+			else
+			{
+				Console.WriteLine("ok!");
+			}
+			Console.WriteLine("nacisnij dowolny klawisz");
+			Console.ReadKey();
+			Console.Clear();
+		}
+		/// <summary>
+		/// Wypisanie powitania
+		/// </summary>
+		private static void Greeting()
+		{
+			Console.Write("wpisz swoje imie ");
+			string name = Console.ReadLine();
+			Console.WriteLine("Witaj " + name);
 		}
 	}
 }
