b7bee654b40bc3324d35569e64970313e5015402
mateuszmiga <mateusz.miga11@gmail.com>
2018-11-21 20:21:18 +0100

    kurs 10

---
+++ Constructors/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+	class Person
+	{
+		int id;
+		string firstName;
+		string lastName;
+		string address;
+		int age;
+		string gender;
+
+
+		public Person()
+		{
+			id = 1;
+			firstName = "matt";
+			lastName = "mig";
+			age = 14;
+
+		}
+
+		public Person(int id, string firstName, string lastName, string address, int age, string gender)
+		{
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			this.age = age;
+			this.gender = gender;
+		}
+
+		public Person(int id, string firstName, string lastNam)
+		{
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			address = "katowice";
+			age = 18;
+
+		}
+		public Person(int id, string firstName)
+		{
+			this.id = id;
+			this.firstName = firstName;
+		}
+	}
+}
